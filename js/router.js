ac6c91e8f0cd07ded872f3fc22b170c89d33d8a3
LUCAS VINICIUS PEREIRA DA SILVA <22282123@CFP127ALUNO.LOCAL>
2023-05-08 10:48:37 -0300

    primeira tela feita

---
+++ js/router.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const routes = {
+    '/'         :   '/pages/home.html',
+    '/characters' :   '/pages/characters.html',
+    '/curiosities'     :   '/pages/curiosities.html',
+}
+
+const route = async () => {
+    window.event.preventDefault()
+    window.history.pushState({}, "", window.event.target.href)
+
+    const path = window.location.pathname
+    const route = routes[path]
+
+    const response = await fetch(route) 
+    const html = await response.text()
+
+    document.getElementById('root').innerHTML = html
+}
+
+window.route = route
