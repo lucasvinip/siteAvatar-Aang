c927f2cfb1f614e559eb135fa01d9ceb8392077e
Lucas Vinicius <l.vinicius1577@gmail.com>
2023-05-10 20:52:18 -0300

    js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,45 @@
 'use strict'
 
 import './router.js'
+
+// const simpsons = async () =>{
+//     const url = `https://thesimpsonsquoteapi.glitch.me/quotes?character=homer simpson`
+
+//     const response = await fetch(url)
+//     const data = await response.json()
+
+//     return data
+// }
+
+// console.log(simpsons([2]));
+
+// const createCharacters = () =>{
+
+//     const characters = document.createElement('div')
+//     characters.classList.add('characters')
+
+//     const name = document.createElement('p')
+//     name.classList.add('name')
+
+//     const circle = document.createElement('div')
+//     circle.classList.add('circle')
+
+//     const arrow = document.getElementById('arrow')
+
+//     const tag = document.createElement('a')
+//     tag.classList.add('container-simpsons')
+
+//     const img = document.createElement('img')
+//     img.classList.add('simpsons')
+
+//     const quote = document.createElement('p')
+//     name.classList.add('famousQuotes')
+
+//     tag.append(img)
+//     circle.append(arrow, tag)
+    
+
+//     return characters
+// }
+
+// createCharacters()
